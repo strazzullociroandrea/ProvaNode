92f1d5165f18944aac750750dce212eedfed7210
ciroandrea <strazzullociroandrea@itis-molinari.eu>
2024-01-08 10:38:39 +0100

    finito

---
+++ main.js
@@ -0,0 +1,11 @@
+
+let http = require("http");//import http
+
+http.createServer(function(request, response){
+    response.writeHead(200,{
+        "content-type": "text/plain"
+    });
+    response.end("Hello world!\n");
+}).listen(8081);
+
+console.log("Server running on PORT 8081");
